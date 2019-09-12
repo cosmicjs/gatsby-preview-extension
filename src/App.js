168ba64cc53fad5e6838f6467e4b24ad567986ee
Tony Spiro <tspiro@tonyspiro.com>
2019-09-12 09:07:59 -0500

    EDITED: styles

--- src/App.js
+++ src/App.js
@@ -11,14 +11,17 @@ export default class App extends Component {
     var object_slug = url.searchParams.get('object_slug');
     window.open(preview_url + '/' + object_slug)
   }
+  handleGatsbyLogoClick = () => {
+    window.open('https://gatsbyjs.com')
+  }
   render() {
     return (
       <div style={{ textAlign: 'center', width: 200, paddingTop: 10 }}>
         <div style={{marginBottom: 10 }}>
           <Button onClick={this.handleClick} primary style={{ width: 200, background: '#00AFD7' }}>Preview&nbsp;&nbsp;&nbsp;&nbsp;<Icon name='external alternate' /></Button>
         </div>
-        <div style={{marginBottom: 10 }}>
-          <div style={{ float: 'left', marginLeft: 40, marginTop: 5 }}>Powered by:</div>
+        <div onClick={this.handleGatsbyLogoClick} style={{marginBottom: 10, width: 200, cursor: 'pointer' }}>
+          <div style={{ float: 'left', marginLeft: 40, marginTop: 5, color: '#586069' }}>Powered by</div>
           <div style={{ float: 'left', marginLeft: 10 }}><img src={gatsby} style={{ width: 30 }}/></div>
         </div>
       </div>
