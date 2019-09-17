290fe3a695bfa1956e15c454c33b14ea1864d6c1
Jazib Sawar <jazibsawar@gmail.com>
2019-09-17 10:49:19 +0300

    Initial Version

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import React, { Component } from 'react';
-import { Button, Icon } from 'semantic-ui-react'
-import 'semantic-ui-css/semantic.min.css'
-import gatsby from './gatsby.svg'
+import React, { useState, useEffect } from 'react';
+import {
+  ExtensionUI,
+  getContentPath
+} from '@gatsby-cloud-pkg/gatsby-cms-extension-base';
 
-export default class App extends Component {
-  handleClick = () => {
-    var url_string = window.location.href
-    var url = new URL(url_string);
-    var preview_url = url.searchParams.get('preview_url');
-    var object_slug = url.searchParams.get('object_slug');
-    window.open(preview_url + '/' + object_slug)
-  }
-  handleGatsbyLogoClick = () => {
-    window.open('https://gatsbyjs.com')
-  }
-  render() {
-    return (
-      <div style={{ textAlign: 'center', width: 200, paddingTop: 5 }}>
-        <div style={{marginBottom: 12 }}>
-          <Button onClick={this.handleClick} primary style={{ width: 200, background: '#00AFD7' }}>Preview&nbsp;&nbsp;&nbsp;&nbsp;<Icon name='external alternate' /></Button>
-        </div>
-        <div onClick={this.handleGatsbyLogoClick} style={{marginBottom: 10, width: 200, cursor: 'pointer' }}>
-          <div style={{ float: 'left', marginLeft: 40, marginTop: 5, color: '#586069' }}>Powered by</div>
-          <div style={{ float: 'left', marginLeft: 10 }}><img src={gatsby} style={{ width: 30 }}/></div>
-        </div>
-      </div>
-    );
-  }
+function App() {
+  const [contentSlug, putContentSlug] = useState('');
+  const [previewUrl, putPreviewUrl] = useState('');
+  const [previewContentUrl, putPreviewContentUrl] = useState('');
+
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    putContentSlug(url.searchParams.get('object_slug') || '');
+    putPreviewUrl(url.searchParams.get('preview_url') || '');
+    async function fetchContentPath(payload) {
+      try {
+        const finalPreviewURl = await getContentPath(payload);
+        putPreviewContentUrl(finalPreviewURl | '');
+      } catch (e) {
+        putPreviewContentUrl('');
+      }
+    }
+    if (contentSlug && previewUrl) {
+      fetchContentPath({ contentSlug, previewUrl, authToken: '' });
+    }
+  }, [contentSlug, previewUrl]);
+
+  return (
+    <>{previewContentUrl && <ExtensionUI previewUrl={previewContentUrl} />}</>
+  );
 }
+
+export default App;
